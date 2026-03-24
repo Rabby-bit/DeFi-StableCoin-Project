33360588dc8f00420298a140962ec305bc8668b3
Rabby-bit <rabiaubaidu@gmail.com>
2026-03-24 15:31:06 +0100

    fix: format code

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -30,72 +30,55 @@ import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-
 contract DSCEngine is ReentrancyGuard {
+    error DSCEngine__ValueShouldBeMoreThanZero();
+    error DSCEngine__NotAnAllowedTokenAddress();
 
-error DSCEngine__ValueShouldBeMoreThanZero();
-error DSCEngine__NotAnAllowedTokenAddress();
-
-
-
-
-modifier moreThanZero(uint256 value ) {
-    if(value == 0) {
-        revert DSCEngine__ValueShouldBeMoreThanZero();
-    }
-    _;
-}
-
-modifier allowedToken(address tokenAddress) {
-    if (s_tokentoPriceFeed[tokenAddress] == address(0)){
-        revert DSCEngine__NotAnAllowedTokenAddress();
-    }
-    _;
-}
-
-mapping (address token => address priceFeed) private s_tokentoPriceFeed;
-mapping(address sender => mapping(address  tokenAddress => uint256 value)) private s_collateralDeposited;
-
-
-
-
-constructor(address[] memory tokenAddress ,address[] memory priceFeed) {
-    for (uint256 i= 0; i < tokenAddress.length ; i++) {
-        s_tokentoPriceFeed[tokenAddress[i]] = priceFeed[i];
+    modifier moreThanZero(uint256 value) {
+        if (value == 0) {
+            revert DSCEngine__ValueShouldBeMoreThanZero();
+        }
+        _;
     }
 
-}
-
-    function depositCollateral(address tokenAddress, uint256 value)  external allowedToken(tokenAddress)
-    moreThanZero(value) 
-    nonReentrant returns (bool) {
-    bool success = IERC20(tokenAddress).transferFrom(msg.sender, address(this), value);
-    s_collateralDeposited[msg.sender][tokenAddress] += value;
-    
-    return true ;
-    } 
-
-     function burnDSC() external {
-
+    modifier allowedToken(address tokenAddress) {
+        if (s_tokentoPriceFeed[tokenAddress] == address(0)) {
+            revert DSCEngine__NotAnAllowedTokenAddress();
+        }
+        _;
     }
 
-    function mintDSC() external {
+    mapping(address token => address priceFeed) private s_tokentoPriceFeed;
+    mapping(address sender => mapping(address tokenAddress => uint256 value)) private s_collateralDeposited;
 
+    constructor(address[] memory tokenAddress, address[] memory priceFeed) {
+        for (uint256 i = 0; i < tokenAddress.length; i++) {
+            s_tokentoPriceFeed[tokenAddress[i]] = priceFeed[i];
+        }
     }
 
-    function redeemCollateral() external {
+    function depositCollateral(address tokenAddress, uint256 value)
+        external
+        allowedToken(tokenAddress)
+        moreThanZero(value)
+        nonReentrant
+        returns (bool)
+    {
+        bool success = IERC20(tokenAddress).transferFrom(msg.sender, address(this), value);
+        s_collateralDeposited[msg.sender][tokenAddress] += value;
 
+        return true;
     }
 
-    function redeemCollateralforDSC( ) external {
+    function burnDSC() external {}
 
-    }
+    function mintDSC() external {}
 
-    function liquidate() external {
+    function redeemCollateral() external {}
 
-    }
+    function redeemCollateralforDSC() external {}
 
-    function getHealthFactor() external view returns (uint256) {
+    function liquidate() external {}
 
-    }
+    function getHealthFactor() external view returns (uint256) {}
 }
--- test/DSCTest.t.sol
+++ test/DSCTest.t.sol
@@ -6,86 +6,64 @@ import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
 import {Test} from "forge-std/Test.sol";
 
 contract DSCTest is Test {
+    DecentralizedStableCoin public DSC;
+    address user;
 
-  DecentralizedStableCoin public DSC;
-  address user;
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
 
-  error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    function setUp() public {
+        address user = makeAddr("user");
+        vm.startPrank(user);
+        DecentralizedStableCoin DSC = new DecentralizedStableCoin();
+        vm.stopPrank();
+    }
 
-  function setUp() public {
-   address user = makeAddr("user");
-   vm.startPrank(user);
-   DecentralizedStableCoin DSC = new DecentralizedStableCoin();
-   vm.stopPrank();
-  
-  }
+    function test__IfBurnWorks() public {
+        //Arrange
+        address user = makeAddr("user");
+        vm.startPrank(user);
+        DecentralizedStableCoin DSC = new DecentralizedStableCoin();
+        DSC.mint(user, 100);
+        DSC.burn(20);
+        vm.stopPrank();
+        //Assert
+        uint256 balance = DSC.balanceOf(user);
+        assertEq(80, balance);
+    }
 
-  function test__IfBurnWorks() public {
-    //Arrange 
-    address user = makeAddr("user");
-    vm.startPrank(user);
-    DecentralizedStableCoin DSC = new DecentralizedStableCoin();
-    DSC.mint(user, 100); 
-    DSC.burn(20);
-    vm.stopPrank();
-    //Assert
-    uint256 balance = DSC.balanceOf(user);
-    assertEq(80, balance);
-}
+    function test__IfItRevert_BurnAmountExceedsBalance() public {
+        address user = makeAddr("user");
+        vm.startPrank(user);
+        DecentralizedStableCoin DSC = new DecentralizedStableCoin();
+        vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__BurnAmountExceedsBalance.selector);
+        DSC.burn(20);
+        vm.stopPrank();
+    }
 
-function test__IfItRevert_BurnAmountExceedsBalance() public {
+    function test__IfItRevert_AmountMustBeMoreThanZero() public {
+        address user = makeAddr("user");
+        vm.startPrank(user);
+        DecentralizedStableCoin DSC = new DecentralizedStableCoin();
+        vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__AmountMustBeMoreThanZero.selector);
+        DSC.burn(0);
+        vm.stopPrank();
+    }
 
-    address user = makeAddr("user");
-    vm.startPrank(user);
-    DecentralizedStableCoin DSC = new DecentralizedStableCoin();
-    vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__BurnAmountExceedsBalance.selector);
-    DSC.burn(20);
-    vm.stopPrank();
+    function test__ifRevert_CantMintToAZeroAddress() public {
+        address user = makeAddr("user");
+        vm.startPrank(user);
+        DecentralizedStableCoin DSC = new DecentralizedStableCoin();
+        vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__CantMintToAZeroAddress.selector);
+        DSC.mint(address(0), 100);
+        vm.stopPrank();
+    }
 
-}
-
-function test__IfItRevert_AmountMustBeMoreThanZero() public {
-    address user = makeAddr("user");
-    vm.startPrank(user);
-    DecentralizedStableCoin DSC = new DecentralizedStableCoin();
-    vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__AmountMustBeMoreThanZero.selector);
-    DSC.burn(0);
-    vm.stopPrank();
-}
-
-function test__ifRevert_CantMintToAZeroAddress() public {
-   address user = makeAddr("user");
-    vm.startPrank(user);
-    DecentralizedStableCoin DSC = new DecentralizedStableCoin();
-    vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__CantMintToAZeroAddress.selector);
-    DSC.mint(address(0), 100); 
-    vm.stopPrank(); 
-}
-
-function test_ifRevert_CantMintZeroTokens() public {
-    address user = makeAddr("user");
-    vm.startPrank(user);
-    DecentralizedStableCoin DSC = new DecentralizedStableCoin();
-    vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__CantMintZeroTokens.selector);
-    DSC.mint(user, 0); 
-    vm.stopPrank();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    function test_ifRevert_CantMintZeroTokens() public {
+        address user = makeAddr("user");
+        vm.startPrank(user);
+        DecentralizedStableCoin DSC = new DecentralizedStableCoin();
+        vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__CantMintZeroTokens.selector);
+        DSC.mint(user, 0);
+        vm.stopPrank();
+    }
 }
